b1da8e7b20c772a2cfbe1a1221777067db502e29
Marian Albu <albumeo1990@gmail.com>
2021-03-19 15:08:51 +0200

    added suport for ios

--- Screens/Timepicker/Timepicker.js
+++ Screens/Timepicker/Timepicker.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react'
-import { View, TextInput, StyleSheet, Platform, Text, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import { View, TextInput, StyleSheet, Platform, Text, TouchableWithoutFeedback, Keyboard, TouchableOpacity } from 'react-native'
 import DateTimePicker from '@react-native-community/datetimepicker'
+import {Overlay} from 'react-native-elements'
 
 const Timepicker = () => {
   const [show, setShow] = useState(false)
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState(new Date())
+  const [inputValue, setInputValue] = useState('')
 
   const handleChange = (event, selectedDate) => {
     const timestamp = selectedDate || new Date()
     const hours = timestamp.getHours()
     const minutes = timestamp.getMinutes()
-    setValue(`${('0' + hours).slice(-2)} : ${('0' + minutes).slice(-2)}`)
-    setShow(Platform.OS !== 'ios')
+    setShow(Platform.OS === 'ios')
+    setInputValue(`${('0' + hours).slice(-2)} : ${('0' + minutes).slice(-2)}`)
+    setValue(selectedDate || new Date())
+  }
+
+  const hideOverlay = () => {
+    setShow(false)
   }
 
   return (
@@ -20,19 +27,52 @@ const Timepicker = () => {
         <TextInput 
           style={styles.input}
           placeholder='Select time'
-          value={value}
+          value={inputValue}
           onFocus={() => setShow(true)}
           onBlur={() => setShow(false)}
         />
-        {
+
+        {Platform.OS === 'ios' ? (
+          <Overlay isVisible={show} onBackdropPress={hideOverlay} style={{
+            flex: 1, 
+            width: '200', 
+            justifyContent: 'flex-end',  
+            backgroundColor: 'transparent',
+          }}>
+            <View style={{
+              borderColor: '#333', 
+              borderBottomWidth: 1, 
+              height: 50, 
+              justifyContent: 'space-between', 
+              alignItems: 'center', 
+              flexDirection: 'row', 
+            }}>
+              <TouchableOpacity onPress={hideOverlay}>
+                <Text style={{ paddingHorizontal: 15 }}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={hideOverlay}>
+                <Text style={{ paddingHorizontal: 15, color: '#ff9980' }}>Done</Text>
+              </TouchableOpacity>
+            </View>
+            <DateTimePicker
+              value={value}
+              mode='time'
+              is24Hour={true}
+              display="default"
+              onChange={handleChange}
+              style={{ width: 200 }}
+            />
+          </Overlay>
+        ) : (
           show && (
             <DateTimePicker
-              value={new Date()}
+              value={value}
               mode='time'
               is24Hour={true}
               display="default"
               onChange={handleChange}
             />
+          )
         )}
       </View>
     </TouchableWithoutFeedback>
